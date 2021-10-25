5ae80b41f1e642793f480f250de07a7b3d2efb7e
Barlowe <riquelme029@gmail.com>
2021-10-25 17:20:31 +0200

    add P5 p2 - initial

--- practica5/p5_p2_codificacion_frase.cpp
+++ practica5/p5_p2_codificacion_frase.cpp
@@ -5,11 +5,39 @@ using namespace std;
 	//frase: caracter;
 //ALGORITMO
 	//Leer(frase);
-	
+	//REPETIR
+		//SEGUN frase
+			//Z: Escribir(A)
+			//z: Escribir(a)
+			//9: Escribir(0)
+			//c: Escribir(siguiente_letra)
+			//EN_OTRO_CASO: Escribir(*)
+		//FIN_SEGUN
+	//HASTA_QUE frase=\n
 //FIN
 
 
 int main(){
 	
+	char frase;
+
+	do{		
+		cin.get(frase);
+
+		if(frase=='Z'){
+			cout << 'A';
+		}else if(frase=='z'){
+			cout << 'a';
+		}else if(frase=='9'){
+			cout << '0';
+		}else if(frase=='(' || frase==')' || frase==' '){
+			cout << '*';
+		}else{
+			(int)frase++;
+			cout << (char)frase;
+		}
+
+	}while(frase!='\n');
+		
 	return 0;
 }
