8f17c840b8c93b13a86754a94e42bbad358b7701
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-11-02 10:46:40 +0100

    added SP

---
+++ practica6/p6_p3.cpp
@@ -0,0 +1,36 @@
+//LEXICO
+	//MarcaFin: constante entero;
+	//secuencia_S: secuencia enteros;
+	//secuencia_T: secuencia enteros;
+	//actual, siguiente, suma :entero;
+//ALGORITMO
+	//Comenzar(s);
+	//actual <- EA(S);
+	//suma <- actual;
+	
+	//MIENTRAS EA(S) != MarcaFin HACER
+		//Escribir(suma);
+		//siguiente <- Avanzar(S);
+		//suma <- suma + siguiente;
+		//Avanzar(S);
+	//FIN_MIENTRAS
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main(){
+	
+	const MARCA_FIN_LINEA = '\n';
+	list <int> S, T;
+	list <int> ::iterator EA_S;
+	int actual, suma, siguiente;
+	
+	while(){
+		
+		
+	}
+	
+	
+	return 0;
+}
