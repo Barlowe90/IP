36a4d4d65cb39cbdf6f4a4d4e301a453ab1aea53
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-11-03 20:14:28 +0100

    version final con 2 tablas

--- practica6/p6_p3.cpp
+++ practica6/p6_p3.cpp
@@ -1,17 +1,23 @@
 //LEXICO
 	//MarcaFin: constante entero;
 	//secuencia_S: secuencia enteros;
+	//secuencia_T: secuencia enteros;
 	//suma: entero;
 //ALGORITMO
 	//Comenzar(s);
 	//suma <- EA(S);
-	//Escribir(suma);
+	//Registrar(suma);
 	//ITERAR
 		//Avanzar(S);
 	//DETENER EA(S) = MarcaFin;
 		// suma <- suma + EA(S);
+		// Registrar(suma);
 	//FIN_ITERAR
-	//Escribir(suma);
+	//Comenzar(T);
+	//MIENTRAS(EA(T) != MarcaFin) HACER
+		//Escribir(EA(T);
+		//Avanzar(T);
+	//FIN_MIENTRAS
 
 #include <iostream>
 #include <string>
@@ -20,24 +26,32 @@ using namespace std;
 
 int main(){
 	
-	list <int> S;
-	list <int>::iterator EA;
+	list <int> S, T;
+	list <int>::iterator EA_S, EA_T;
 	int suma;
 
 	S = {1, 4, 5, 3, 5, 6};
 	
-	EA = S.begin();
-	
-	suma = *EA;		
-	cout << suma << ", ";
+	EA_S = S.begin();
+
+	suma = *EA_S;		
+	T.push_back(suma);
 	
 	do{
-		EA++;
-		if(EA == S.end())
+		EA_S++;
+		if(EA_S == S.end())
 			break;
-			suma = suma + *EA;
-			cout << suma << ", ";
+			suma = suma + *EA_S;
+			T.push_back(suma);
 	}while(true);
+	
+	// recorrer y mostrar T
+	EA_T = T.begin(); 
+
+	while(EA_T != T.end()){
+		cout << *EA_T << ", ";
+		EA_T++;
+	}
 			
 	return 0;
 }
