ec0151a5dff27991016b6f9665a97e781933ce95
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-11-03 19:29:13 +0100

    SP practica 6 problema 4

---
+++ practica6/p6_p4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <string>
+
+//ALGORITMO
+//	Comenzar(S1);
+//	Comenzar(S2);
+//  MIENTRAS(S1 O S2 != MarcaFin)
+//		SI S1==S2 ENTONCES
+//			Registrar(R, EA_S1);
+//			Avanzar(EA_S1);
+//			Avanzar(EA_S2);
+//  	SI S1>S2 ENTONCES	
+//			Avanzar(EA_S2);
+// 	 	SI S2>S1 ENTONCES	
+//			Avanzar(EA_S1);
+// 	FIN_MIENTRAS
+
+
+using namespace std;
+
+int main(){		
+		
+	return 0;
+}
