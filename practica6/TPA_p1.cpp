7f677dc2f08583f03d1f302707dab893a52d781d
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-11-09 10:27:35 +0100

    Add SP

---
+++ practica6/TPA_p1.cpp
@@ -0,0 +1,11 @@
+//LEXICO
+	//j1, j2, entero;
+	//marcadorActual: secuencia;
+	//marcadorNuevo: funcion (marcadorActual: secuencia, jugador: entero) -> caracter;
+//ALGORITMO
+	//Leer(marcador);
+	//MIENTRAS(EA != MarcaFin) HACER
+		//*EA // Sacar marcador y jugador
+		//marcadorNuevo <- (marcadorActual, jugador);
+	//FIN_MIENTRAS
+	
