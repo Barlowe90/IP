89899184e7b93f4ee429018e0d6381681bc01eb1
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-11-23 10:40:15 +0100

    initial problem

---
+++ practica8/p7_p1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	const int N = 10;
+	typedef int Vector[N];
+	int i;
+	
+	Vectot t = {1, 2, 3, 4, 5};
+	Vector z;
+	
+	// mirar esquemas iterativos, tratar primer elemento difernte del resto
+	
+	return 0;
+}
