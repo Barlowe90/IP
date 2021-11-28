8e387ab8aef9d0e1dbcac62b1701f0b91929cc68
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-11-28 19:12:04 +0100

    add SC - working

--- practica6/p6_p3.cpp
+++ practica6/p6_p3.cpp
@@ -1,57 +1,60 @@
-//LEXICO
-	//MarcaFin: constante entero;
-	//secuencia_S: secuencia enteros;
-	//secuencia_T: secuencia enteros;
-	//suma: entero;
-//ALGORITMO
-	//Comenzar(s);
-	//suma <- EA(S);
-	//Registrar(suma);
-	//ITERAR
-		//Avanzar(S);
-	//DETENER EA(S) = MarcaFin;
-		// suma <- suma + EA(S);
-		// Registrar(suma);
-	//FIN_ITERAR
-	//Comenzar(T);
-	//MIENTRAS(EA(T) != MarcaFin) HACER
-		//Escribir(EA(T);
-		//Avanzar(T);
-	//FIN_MIENTRAS
+//~ LEXICO
+	//~ S1, S2, R: secuencia de Entero;
+//~ ALGORITMO
+	//~ Comenzar(S1); Comenzar(S2);
+	//~ MIENTRAS EA(S1) != MarcaFin Y EA(S2) != MarcaFin HACER
+		//~ SI EA(S1) < EA(S2) ENTONCES	
+			//~ Avanzar (S1);
+		//~ FIN_SI
+		//~ SI EA(S2) < EA(S1) ENTONCES	 
+			//~ Avanzar (S1);
+		//~ FIN_SI
+		//~ SI EA(S1) = EA(S2) ENTONCES	
+			//~ Registrar(R,S1);
+		//~ FIN_SI
+	//~ FIN_MIENTRAS
+	
+	//~ Comenzar(R)
+	//~ MIENTRAS EA(R) != MarcaFin HACER
+		//~ Escribir(EA(R);
+		//~ Avanzar(R);
+	//~ FIN_MIENTRAS;
+//~ FIN
 
 #include <iostream>
-#include <string>
 #include <list>
 using namespace std;
 
 int main(){
 	
-	list <int> S, T;
-	list <int>::iterator EA_S, EA_T;
-	int suma;
-
-	S = {1, 4, 5, 3, 5, 6};
+	list <int> S1, S2, R;
+	list <int>::iterator EA_S1, EA_S2, EA_R;
 	
-	EA_S = S.begin();
-
-	suma = *EA_S;		
-	T.push_back(suma);
+	S1 = {-44, -21, -1, 3, 5, 7, 18, 24, 30, 33, 54, 66, 101};
+	S2 = {-32, -28, -21, 0, 3, 4, 6, 7, 14, 20, 24, 31, 33, 51, 66, 104, 200};
 	
-	do{
-		EA_S++;
-		if(EA_S == S.end())
-			break;
-			suma = suma + *EA_S;
-			T.push_back(suma);
-	}while(true);
-	
-	// recorrer y mostrar T
-	EA_T = T.begin(); 
+	EA_S1 = S1.begin();
+	EA_S2 = S2.begin();
 
-	while(EA_T != T.end()){
-		cout << *EA_T << ", ";
-		EA_T++;
+	while(EA_S1 != S1.end() && EA_S2 != S2.end()){
+		if(*EA_S1 < *EA_S2){
+			EA_S1++;
+		}else if(*EA_S2 < *EA_S1){
+			EA_S2++;
+		}else if(*EA_S1 == *EA_S2){
+			R.push_back(*EA_S1);
+			EA_S1++;
+		}
 	}
-			
+	
+	// recorrer y mostrar R
+	EA_R = R.begin(); 
+
+	cout << "[ "; 
+	while(EA_R != R.end()){
+		cout << *EA_R << ", ";
+		EA_R++;
+	}
+	cout << " ]";		
 	return 0;
 }
