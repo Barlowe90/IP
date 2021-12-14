178aaca8657f9d6130abe3d290a081ab97e718af
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-12-14 10:41:19 +0100

    final practica 8 problema 1

--- practica8/p8_p1.cpp
+++ practica8/p8_p1.cpp
@@ -5,20 +5,20 @@ int main(){
 	
 	const int N = 10;
 	typedef int Vector[N];
-	int i;
+	int i, ultimo;
 	
 	Vector t = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
 	
-	t[1] = t[N-1];
+	ultimo = t[N-1];
 	
-	for (i = N-2; i >= 1; --i) {
-		t[i] = t[i+1];
+	for (i = N-1; i >= 1; --i) {
+		t[i] = t[i-1];
+	}
+	t[0] = ultimo;
+
+	for(i = 0; i< N; i++){
 		cout << t[i] << ", ";
 	}
-
 	
-	
-	
-		
 	return 0;
 }
